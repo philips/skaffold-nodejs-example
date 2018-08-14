1ad18fb9074eca430f15e5ec0f1acea739298d0a
Brandon Philips <bphilips@redhat.com>
2018-08-14 10:20:00 -0700

    adds

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require('express')
 const app = express()
 
+var counter = 1
+
 app.get('/', function (req, res) {
   res.json({
-    status: 'ok'
+    counter: counter
+  })
+})
+
+app.get('/increment', function (req, res) {
+  counter = counter + 1
+  res.json({
+    counter: counter
+  })
+})
+
+app.get('/decrement', function (req, res) {
+  counter = counter - 1
+  res.json({
+    counter: counter
   })
 })
 
